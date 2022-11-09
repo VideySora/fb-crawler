c6de7a14284aef066104e7f5bf0276c31d8a63f0
Anh Huan <huan.cao2909@hcmut.edu.vn>
2022-11-09 14:09:01 +0700

    Add logout and system can show projects of particular user

--- controllers/user.js
+++ controllers/user.js
@@ -118,16 +118,18 @@ const secret = async (req, res, next) => {
 
 const signIn = async (req, res, next) => {
     console.log('Called to signIn function')
-    // const { username, password } = req.body
+    const { username, password } = req.value.body
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
 
-    // // Check users
-    // const foundUser = await User.findOne({ username, password })
-    // if (foundUser) res.redirect('/home')
-    // else return res.status(403).json({ error: { message: 'Username not found!' } })
-    const user = await User.findOne({username: req.body.username});
-    if (!user) return res.status(422).send('Username or Password is not correct');
+    // Check users
+    const foundUser = await User.findOne({ username, hashPassword })
+    if (foundUser) res.redirect('/home')
+    else return res.status(403).json({ error: { message: 'Username or Password is not correct!' } })
+    // const user = await User.findOne({username: req.body.username});
+    // if (!user) return res.status(422).send('Username or Password is not correct');
 
-    const checkPassword = await bcrypt.compare(req.body.password, user.password);
+    // const checkPassword = await bcrypt.compare(req.body.password, user.password);
 
     if (!checkPassword) return res.status(422).send('Email or Password is not correct');
     return res.redirect("/home");
@@ -136,21 +138,21 @@ const signIn = async (req, res, next) => {
 
 const signUp = async (req, res, next) => {
     console.log('Called to signUp function')
-    const username = req.body.username;
+    const { username, email, password } = req.value.body;
 
     // Check users
-    const foundUser = await User.findOne({ username })
+    const foundUser = await User.findOne({ username, email })
     if (foundUser) return res.status(403).json({ error: { message: 'Username has already been used!' } })
 
     const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    const hashPassword = await bcrypt.hash(password, salt);
 
     // Create new user
-    const newUser = new User({ username: req.body.username,
+    const newUser = new User({ username: username, email: email, 
                                 password: hashPassword })
     await newUser.save()
 
-    //res.status(201).send("Register successful.");
+    // res.status(201).send("Register successful.");
     return res.redirect("/login");
 }
 
